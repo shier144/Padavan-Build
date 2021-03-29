23db0badac0e3fc7b45ed6f7dc739280e62b21ac
shier <58193016+shier144@users.noreply.github.com>
2021-03-29 15:03:15 +0800

    Update PSG1218.sh

--- public/PSG1218.sh
+++ public/PSG1218.sh
@@ -4,12 +4,12 @@
 # 修改默认参数（不同设备拷贝到相应 *.sh)          by: TurBoTse
 #####################################################################
 
-lan_ip="192.168.2"                          # LAN 地址 别写后面的 .1
+lan_ip="192.168.1"                          # LAN 地址 别写后面的 .1
 default_path="./user/shared"                # 默认配置路径
 config="./configs/templates/PSG1218.config" # 默认配置文件
 
 echo "修改 LAN IP 地址"
-sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
+sed -i "s/192.168.1/$lan_ip/g" $default_path/defaults.h
 
 # echo "设置为PPPOE模式并写入账号和密码"
 # sed -i 's/{ "wan_proto", "dhcp" }/{ "wan_proto", "pppoe" }/g' $default_path/defaults.c
